088c5306bc1a7aab6b9a9f36059305546e2e286a
Rick Hall <rickhall2000@hotmail.com>
2019-05-08 12:29:51 -0400

    Main function requires 1 argument

--- src/file_parser/core.clj
+++ src/file_parser/core.clj
@@ -1,5 +1,12 @@
 (ns file-parser.core)
 
+(defn doer-function
+  [arg]
+  (str "this is pointless " arg))
+
 (defn -main
   [& args]
-  (println args))
+  (if
+    (= (count args) 1)
+    (doer-function (first args))
+    (println "Please pass a single argument, containing the filename you wish to load")))
--- test/file_parser/core_test.clj
+++ test/file_parser/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [file-parser.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest main-only-works-with-one-arg-test
+  (with-redefs [file-parser.core/doer-function
+                (constantly true)]
+    (testing "The main function calls the doer when it has 1 argument"
+      (is (true? (-main "test"))))
+    (testing "The main function does not call the doer when it has no arguments"
+      (is (nil? (-main))))
+    (testing "The main function does not call the doer when it has more than 1 argument"
+      (is (nil? (-main "one" "two" "three"))))))
